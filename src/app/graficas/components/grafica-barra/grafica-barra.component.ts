516261d8a3a8b68dfee9e9b713da16ea121d4189
Belkis <katerinne65@gmail.com>
2022-04-23 15:39:05 +0200

    Clase 337 Añadir flexibilidad a nuestro componente personalizado

--- src/app/graficas/components/grafica-barra/grafica-barra.component.ts
+++ src/app/graficas/components/grafica-barra/grafica-barra.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { ChartConfiguration, ChartData, ChartType, ChartEvent } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
 
@@ -12,11 +12,13 @@ import DataLabelsPlugin from 'chartjs-plugin-datalabels';
 })
 export class GraficaBarraComponent implements OnInit {
 
+  @Input() horizontal: boolean = false;
   @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
 
   public barChartOptions: ChartConfiguration['options'] = {
 
     responsive: true,
+    indexAxis: 'x',
     // We use these empty structures as placeholders for dynamic theming.
     scales: {
       x: {},
@@ -40,18 +42,28 @@ export class GraficaBarraComponent implements OnInit {
     DataLabelsPlugin
   ];
 
-  public barChartData: ChartData<'bar'> = {
-    labels: [ '2006', '2007', '2008', '2009', '2010', '2011', '2012' ],
+  @Input()public barChartData: ChartData<'bar'> = {
+
+    //labels: [ '2006', '2007', '2008', '2009', '2010', '2011', '2012' ],
     datasets: [
-      { data: [ 65, 59, 80, 81, 56, 55, 40 ], label: 'Series A', backgroundColor: '#FC7B6F', hoverBackgroundColor: 'red' },
+      /*{ data: [ 65, 59, 80, 81, 56, 55, 40 ], label: 'Series A', backgroundColor: '#FC7B6F', hoverBackgroundColor: 'red' },
       { data: [ 28, 48, 40, 19, 86, 27, 90 ], label: 'Series B', backgroundColor: '#8C5FD9', hoverBackgroundColor: 'red' },
       { data: [ 18, 58, 20,  9, 26, 77, 20 ], label: 'Series C', backgroundColor: '#75E8F0', hoverBackgroundColor: 'red' }
+      */
     ]
   };
 
   constructor() { }
 
   ngOnInit(): void {
+    if (this.horizontal){
+      //Para invertir las barras y colocarlas horizontalmente necesitamos cambiar el "indexAxis"
+      this.barChartOptions!.indexAxis = 'y';
+
+      //Para que se nos muestren todos los datos horizontalmente también debemos retirar el
+      //"min" que tenemos en el barChartOptions.
+      this.barChartOptions!.scales!["y"]!.min = 0;
+    }
   }
 
   public chartClicked({ event, active }: { event?: ChartEvent, active?: {}[] }): void {
--- src/app/graficas/pages/barras-doble/barras-doble.component.ts
+++ src/app/graficas/pages/barras-doble/barras-doble.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
+
+import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
+
 
 @Component({
   selector: 'app-barras-doble',
@@ -6,11 +10,33 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BarrasDobleComponent implements OnInit {
+export class BarrasDobleComponent {
 
-  constructor() { }
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
 
-  ngOnInit(): void {
-  }
+  public barChartType: ChartType = 'bar';
+
+
+  @Input() public proveedoresData: ChartData<'bar'> = {
+
+    labels: ['2021', '2022','2023','2024','2025'],
+    datasets: [
+
+      { data: [ 100,200,300,400,500 ], label: 'Vendedor A' },
+      { data: [ 50,250,30, 450,200 ], label: 'Vendedor B' },
+    ]
+
+  };
+
+  @Input() public productosData: ChartData<'bar'> = {
+
+    labels: ['2021', '2022','2023','2024','2025'],
+    datasets: [
+      { data: [ 200, 300,400,300, 100 ], label: 'Carros', backgroundColor: 'blue' },
+    ]
+
+  };
+
+
 
 }
