f377108cb0e797cb673df547d3b0d6adcaec9bf7
Belkis <katerinne65@gmail.com>
2022-04-23 21:48:51 +0200

    Clase 341 Cambiar la información mediante RXJS

--- src/app/graficas/pages/dona-http/dona-http.component.ts
+++ src/app/graficas/pages/dona-http/dona-http.component.ts
@@ -13,7 +13,7 @@ export class DonaHttpComponent implements OnInit {
   constructor(private graficasService: GraficasService) { }
 
   ngOnInit(): void {
-    this.graficasService.getUsuariosRedesSociales()
+   /* this.graficasService.getUsuariosRedesSociales()
       .subscribe( data => {
         const labels = Object.keys(data);
         const values = Object.values(data);
@@ -22,7 +22,16 @@ export class DonaHttpComponent implements OnInit {
           datasets: [{ data : values }]
         }
       }
-    )
+    )*/
+    this.graficasService.getUsuariosRedesSocialesDonaData()
+      .subscribe( ({labels, values}) =>{
+        this.doughnutChartData={
+          labels:labels,
+          datasets: [{ data : values }]
+        }
+
+      })
+
   }
 
   public doughnutChartLabels: string[] = [/* 'Download Sales', 'In-Store Sales', 'Mail-Order Sales' */];
