6e7eb32d4d8192d0fecb93a1e52b3829e79afd74
Belkis <katerinne65@gmail.com>
2022-04-23 16:31:19 +0200

    Clase 338 Gráfica de Dona

--- src/app/graficas/pages/dona/dona.component.ts
+++ src/app/graficas/pages/dona/dona.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ChartData, ChartType, Color } from 'chart.js';
 
 @Component({
   selector: 'app-dona',
@@ -6,11 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class DonaComponent implements OnInit {
+export class DonaComponent {
 
-  constructor() { }
+  public doughnutChartLabels: string[] = [ 'Download Sales', 'In-Store Sales', 'Mail-Order Sales' ];
 
-  ngOnInit(): void {
-  }
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: this.doughnutChartLabels,
+    datasets: [
+      { data: [ 350, 450, 100 ], backgroundColor: ['#6405F0','#0724E3', '#05A0F0'], hoverBackgroundColor: ['#08FC3F', '#E3FC3F', '#E34CEE'] },/*
+      { data: [ 50, 150, 120 ] },
+      { data: [ 250, 130, 70 ] }*/
+
+    ]
+
+  };
+  public doughnutChartType: ChartType = 'doughnut';
+
 
 }
