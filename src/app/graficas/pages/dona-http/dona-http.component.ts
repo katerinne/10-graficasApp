606b98c303bbea14aad1e92efeff507072a35572
Belkis <katerinne65@gmail.com>
2022-04-23 21:37:04 +0200

    Clase 340 Mostrar la información en la gráfica

--- src/app/graficas/pages/dona-http/dona-http.component.ts
+++ src/app/graficas/pages/dona-http/dona-http.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartData, ChartType } from 'chart.js';
 import { GraficasService } from '../../services/graficas.service';
 
 @Component({
@@ -14,8 +15,26 @@ export class DonaHttpComponent implements OnInit {
   ngOnInit(): void {
     this.graficasService.getUsuariosRedesSociales()
       .subscribe( data => {
-        console.log(data);
-      })
+        const labels = Object.keys(data);
+        const values = Object.values(data);
+        this.doughnutChartData={
+          labels:labels,
+          datasets: [{ data : values }]
+        }
+      }
+    )
   }
+
+  public doughnutChartLabels: string[] = [/* 'Download Sales', 'In-Store Sales', 'Mail-Order Sales' */];
+
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: this.doughnutChartLabels,
+    datasets: [
+    /*  { data: [ 350, 450, 100 ], backgroundColor: ['#6405F0','#0724E3', '#05A0F0'], hoverBackgroundColor: ['#08FC3F', '#E3FC3F', '#E34CEE'] },
+      */
+    ]
+
+  };
+  public doughnutChartType: ChartType = 'doughnut';
 
 }
--- src/app/graficas/pages/dona/dona.component.ts
+++ src/app/graficas/pages/dona/dona.component.ts
@@ -12,12 +12,13 @@ export class DonaComponent {
   public doughnutChartLabels: string[] = [ 'Download Sales', 'In-Store Sales', 'Mail-Order Sales' ];
 
   public doughnutChartData: ChartData<'doughnut'> = {
+
     labels: this.doughnutChartLabels,
     datasets: [
-      { data: [ 350, 450, 100 ], backgroundColor: ['#6405F0','#0724E3', '#05A0F0'], hoverBackgroundColor: ['#08FC3F', '#E3FC3F', '#E34CEE'] },/*
-      { data: [ 50, 150, 120 ] },
-      { data: [ 250, 130, 70 ] }*/
-
+     { data: [ 350, 450, 100 ], backgroundColor: ['#6405F0','#0724E3', '#05A0F0'], hoverBackgroundColor: ['#08FC3F', '#E3FC3F', '#E34CEE'] },
+     /* { data: [ 50, 150, 120 ] },
+      { data: [ 250, 130, 70 ] }
+      */
     ]
 
   };
